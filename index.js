6be0aa9217391aaa5ff5fb29eb36cebd44165ffe
nevomuz-ops <nevomuz@gmail.com>
2026-05-14 13:43:21 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-    index.js
+const TelegramBot = require("node-telegram-bot-api");
+
+const token = "BOT_TOKENINGIZ";
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+
+  if (msg.photo) {
+    bot.sendMessage(chatId, "📄 Chek qabul qilindi. Hozircha test ishlayapti.");
+  }
+
+  if (msg.text === "/start") {
+    bot.sendMessage(chatId, "👋 Bot ishladi. Chek rasmini yuboring.");
+  }
+});
